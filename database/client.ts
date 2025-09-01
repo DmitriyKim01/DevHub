95b16fe163b72067d38dab186070a0b58081266a
dmitriykim01 <dmitriykim.dev@protonmail.com>
2025-08-31 21:40:03 -0400

    Finish setting up the user

--- database/client.ts
+++ database/client.ts
@@ -3,10 +3,6 @@ import * as schema from './schema';
 
 export { sql, eq, and, or } from 'drizzle-orm';
 
-export const tables = schema;
-
 export function useDrizzle() {
   return drizzle(hubDatabase(), { schema });
 }
-
-export type User = typeof schema.users.$inferSelect;
